aa348e69af4fc035dd917b3cb5d050a4f96942c5
Brendan Welzien <brendanwelzien@gmail.com>
2020-12-17 19:24:35 -0800

    test

--- bread/tests.py
+++ bread/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from .models import Bread
+from django.contrib.auth import get_user_model
 
 # Create your tests here.
+class TestBread(TestCase):
+    def test_name(self):
+        name = get_user_model().objects.create_user(
+            name="Daves Killer Bread",
+            description='nine grain oats',
+            bread_type='wheat'
+        )
+        self.assertIsInstance(name, Bread)
+        self.assertEqual(name.bread_type, 'wheat')
+        
